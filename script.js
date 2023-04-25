6fe97355fb6173a32e3f24a5f37048bd8d788944
Iliya <garon7777@ya.ru>
2023-04-25 17:51:36 +0300

    separate XHRequest and UserService

--- script.js
+++ script.js
@@ -1,30 +1,92 @@
-class UserService {
-    #_password;
-    #_username;
-    #_url = "https://examples.com/api/authenticate"
-
-    constructor({username, password}) {
-        this._username = username;
-        this._password = password;
+// Pure JS
+class XHRequest {
+    #reqType;
+    #url;
+    #onLoad;
+    #onError;
+    constructor({url,reqType, onLoad, onError, data}) {
+        this.#url = url
+        this.#reqType = reqType?reqType:"POST"
+        this.#onLoad = onLoad
+        this.#onError = onError
+        
+        if (data) {
+            this.send(data)
+        }
     }
+    send(data) {
+        
+        const xhr = new XMLHttpRequest()
 
-    get username() {return this._username }
-    set username(v) {}
-
-    authenticateUser({done, err}) {
-        let xhr = new XMLHttpRequest()
-        xhr.open("POST", this._url)
-        xhr.send({username: this._username, password: this._password})
         xhr.onload = ()=>{
             switch (xhr.status) {
                 case 200:
-                    done(xhr.response)
+                    this.#onLoad(xhr.response)
                     break;
             
                 default:
-                    err(`${xhr.status}: ${xhr.statusText}`)
+                    if (this.#onError) 
+                        this.#onError({
+                            status: xhr.status,
+                            statusText: xhr.statusText,
+                            response: xhr.response
+                        })
+                    break;
+            }
+        }     
+        
+        switch (this.#reqType) {
+            case "POST":
+                xhr.open("POST", this.#url)
+                xhr.send(data)
+                break;
+            case "GET":
+                xhr.open("GET", this.#url + this.objToGet(data))
+                xhr.send()
+            default:
+                return;
+        }
+        
+ 
+    }
+    objToGet(object){
+        let req = "?"
+        for (let key in object) {
+            if (Object.hasOwnProperty.call(object, key)) {
+                req += `${key}=${object[key]}&`
             }
         }
+        return req;
+    }
+}
+
+class UserService {
+    #password;
+    #username;
+    #url = "https://reqres.in/api/users"
+
+    constructor({username, password}) {
+        this.#username = username;
+        this.#password = password;
+    }
+
+    get username() {return this.#username }
+    set username(v) {}
+
+    authenticateUser({done, err}) {
+        
+        const req = new XHRequest({
+            url: this.#url,
+            reqType: "POST",
+            onLoad: done,
+            onError: err
+        })
+
+        req.send({
+            username: this.#username,
+            password: this.#password
+        })
+
     }
 }
 
@@ -35,11 +97,10 @@ document.querySelector('#login').addEventListener('click', auth);
 function auth() {
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value
-
+    
     const User = new UserService({username, password})
-
     User.authenticateUser({
-        done: ()=>document.location.href = '/home',
+        done: ()=>document.location.href = '/?',
         err: (errText)=> {
             alert('authentication error')
             console.log(errText);
